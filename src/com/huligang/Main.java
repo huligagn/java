b23864e2c2e3251c839698929e041d208915fb9c
huligang <huligang234@163.com>
2018-06-23 22:20:28 +0800

    复习一下剑指offer的习题

--- src/com/huligang/Main.java
+++ src/com/huligang/Main.java
@@ -1,8 +1,33 @@
 package com.huligang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+
+            map.put(numbers[i], i+1);
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        int [] nums = {2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0]+", "+result[1]);
     }
 }
---
+++ src/com/huligang/offer/TreeNode.java
@@ -0,0 +1,10 @@
+package com.huligang.offer;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
