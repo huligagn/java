ab03986414d8cd762bc78a7740018658f5f98838
huligang <huligang234@163.com>
2018-08-01 22:09:44 +0800

    阿里光明小学，没有测试

--- src/com/huligang/Main.java
+++ src/com/huligang/Main.java
@@ -1,33 +1,57 @@
 package com.huligang;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
-    public static int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
+    private static class Point{
+        int px;
+        int py;
+        boolean visited;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (map.containsKey(target - numbers[i])) {
-                result[1] = i + 1;
-                result[0] = map.get(target - numbers[i]);
-                return result;
-            }
-
-            map.put(numbers[i], i+1);
+        Point(int px, int py) {
+            this.px = px;
+            this.py = py;
+            this.visited = false;
         }
 
-        return result;
+        int getLength(Point p){
+            return Math.abs(px - p.px) + Math.abs(py - p.py);
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    private static final Point START = new Point(0,0);
+    private static int shortestPath = Integer.MAX_VALUE;
 
-        int [] nums = {2,7,11,15};
-        int target = 9;
-        int[] result = twoSum(nums, target);
-        System.out.println(result[0]+", "+result[1]);
+    private static int calculate(Point start, Point[] points, int sum, int count){
+
+        if(count == points.length){
+            shortestPath = Math.min(shortestPath, sum + start.getLength(START));
+            return shortestPath;
+        }
+        for (Point point : points) {
+            if (!point.visited) {
+                sum += point.getLength(start);
+                if (sum < shortestPath) {
+                    point.visited = true;
+                    calculate(point, points, sum, count + 1);
+                }
+                sum -= point.getLength(start);
+                point.visited = false;
+            }
+        }
+        return shortestPath;
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = Integer.parseInt(input.nextLine().trim());
+        Point[] points = new Point[N];
+        for(int i = 0; i<N; i++){
+            String[] locations = input.nextLine().trim().split(",");
+            points[i] = new Point(Integer.parseInt(locations[0]), Integer.parseInt(locations[1]));
+        }
+        int min = calculate(START, points, 0, 0);
+        System.out.println(min);
     }
 }
